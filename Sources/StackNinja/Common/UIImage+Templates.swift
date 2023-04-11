b8613907bc6071c94f8f101e1b099837895873b5
AlexSolo <setixela@gmail.com>
2023-04-11 19:41:37 +0300

    wip

--- Sources/StackNinja/Common/UIImage+Templates.swift
+++ Sources/StackNinja/Common/UIImage+Templates.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Aleksandr Solovyev on 28.03.2023.
 //
@@ -8,11 +8,15 @@
 import UIKit
 
 public extension UIImage {
-    var alwaysTemplate: UIImage {
-        withRenderingMode(.alwaysTemplate)
-    }
+   var alwaysTemplate: UIImage {
+      withRenderingMode(.alwaysTemplate)
+   }
 
-    var alwaysOriginal: UIImage {
-        withRenderingMode(.alwaysOriginal)
-    }
+   var alwaysOriginal: UIImage {
+      withRenderingMode(.alwaysOriginal)
+   }
+
+   func oriented(_ orientation: UIImage.Orientation) -> UIImage {
+      .init(cgImage: self.cgImage!, scale: self.scale, orientation: orientation)
+   }
 }
