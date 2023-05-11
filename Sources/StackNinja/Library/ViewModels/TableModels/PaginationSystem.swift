a1e3f265ea865946b93a68b6b1ecda25e7bf0c54
AlexSolo <setixela@gmail.com>
2023-05-12 00:58:09 +0200

    pagination with requestData

--- Sources/StackNinja/Library/ViewModels/TableModels/PaginationSystem.swift
+++ Sources/StackNinja/Library/ViewModels/TableModels/PaginationSystem.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Aleksandr Solovyev on 06.02.2023.
 //
@@ -13,45 +13,50 @@ public struct Pagination {
    public let limit: Int
 }
 
+public struct PaginationWithRequest<Request> {
+   public let offset: Int
+   public let limit: Int
+   public let request: Request
+}
+
 public final class PaginationSystem {
-   
    private lazy var retainer = Retainer()
-   
+
    public var isReady: Bool { !isFinished && !isPaginationInProgress }
-   
+
    private var isPaginationInProgress = false
    private var isFinished = false
    private var currentOffset = 0
-   
+
    private let startOffset: Int
-   private let pageSize: Int 
-   
+   private let pageSize: Int
+
    public init(pageSize: Int, startOffset: Int) {
       self.pageSize = pageSize
       self.startOffset = startOffset
-      self.currentOffset = startOffset
+      currentOffset = startOffset
    }
-   
+
    public func getCurrentPage() -> Pagination {
       isPaginationInProgress = true
-      
+
       return Pagination(offset: currentOffset, limit: pageSize)
    }
-   
+
    public func pageLoaded() {
       isPaginationInProgress = false
       currentOffset += 1
    }
-   
+
    public func pageLoadError() {
       isPaginationInProgress = false
    }
-   
+
    public func finished() {
       isFinished = true
       isPaginationInProgress = false
    }
-   
+
    public func reInit() {
       isFinished = false
       currentOffset = startOffset
@@ -62,9 +67,9 @@ public extension PaginationSystem {
    func paginationForWork<T>(_ loadWork: In<Pagination>.Out<[T]>?) -> Out<[T]> {
       let work = Out<[T]> { [weak self] work in
          guard let self, self.isReady else { work.success([]); return }
-         
+
          let pagination = self.getCurrentPage()
-         
+
          loadWork?
             .doAsync(pagination)
             .onSuccess {
@@ -81,6 +86,41 @@ public extension PaginationSystem {
                work.fail()
             }
       }
+
+      return work.retainBy(retainer)
+   }
+
+   func paginationForWork<T, R>(
+      _ loadWork: In<PaginationWithRequest<R>>.Out<[T]>?,
+      withRequest request: R
+   ) -> Out<[T]> {
+      let work = Out<[T]> { [weak self] work in
+         guard let self, self.isReady else { work.success([]); return }
+
+         let pagination = self.getCurrentPage()
+         let withRequest = PaginationWithRequest(
+            offset: pagination.offset,
+            limit: pagination.limit,
+            request: request
+         )
+
+         loadWork?
+            .doAsync(withRequest)
+            .onSuccess {
+               if $0.isEmpty {
+                  self.finished()
+                  work.cancel()
+               } else {
+                  self.pageLoaded()
+                  work.success($0)
+               }
+            }
+            .onFail {
+               self.pageLoadError()
+               work.fail()
+            }
+      }
+
       return work.retainBy(retainer)
    }
 }
