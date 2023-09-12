2b992d162114612c15858967fd3940fefc2aed06
Setixela <setixela@gmail.com>
2023-09-12 15:54:38 +0200

    fix animations

--- Sources/StackNinja/Library/Setters/View+Set.swift
+++ Sources/StackNinja/Library/Setters/View+Set.swift
@@ -1,19 +1,19 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Aleksandr Solovyev on 03.02.2023.
 //
 
-import UIKit
 import Anchorage
+import UIKit
 
 public extension ViewSetterProtocol {
    @discardableResult func backColor(_ value: UIColor) -> Self {
       view.backgroundColor = value
       return self
    }
-   
+
    @discardableResult func cornerRadius(_ value: CGFloat) -> Self {
       view.layer.cornerRadius = value
       return self
@@ -28,12 +28,12 @@ public extension ViewSetterProtocol {
       view.layer.borderWidth = value
       return self
    }
-   
+
    @discardableResult func borderColor(_ value: UIColor) -> Self {
       view.layer.borderColor = value.cgColor
       return self
    }
-   
+
    @discardableResult func size(_ value: CGSize) -> Self {
       width(value.width)
       height(value.height)
@@ -42,7 +42,7 @@ public extension ViewSetterProtocol {
 
    @discardableResult func circleWithDiameter(_ value: CGFloat) -> Self {
       size(.init(width: value, height: value))
-      cornerRadius(value/2)
+      cornerRadius(value / 2)
       return self
    }
 
@@ -50,12 +50,12 @@ public extension ViewSetterProtocol {
       view.addAnchors.constHeight(value)
       return self
    }
-   
+
    @discardableResult func height(_ value: CGFloat, priority: UILayoutPriority = .defaultHigh) -> Self {
       view.addAnchors.constHeight(value).priority(priority)
       return self
    }
-   
+
    @discardableResult func width(_ value: CGFloat) -> Self {
       view.addAnchors.constWidth(value)
       return self
@@ -65,38 +65,38 @@ public extension ViewSetterProtocol {
       view.addAnchors.constWidth(value).priority(priority)
       return self
    }
-   
+
    @discardableResult func maxHeight(_ value: CGFloat) -> Self {
       view.addAnchors.maxHeight(value)
       return self
    }
-   
+
    @discardableResult func maxWidth(_ value: CGFloat) -> Self {
       view.addAnchors.maxWidth(value)
       return self
    }
-   
+
    @discardableResult func minHeight(_ value: CGFloat) -> Self {
       view.addAnchors.minHeight(value)
       return self
    }
-   
+
    @discardableResult func minWidth(_ value: CGFloat) -> Self {
       view.addAnchors.minWidth(value)
       return self
    }
-   
+
    @discardableResult func sizeAspect(_ value: CGSize) -> Self {
       widthAspect(value.width)
       heightAspect(value.height)
       return self
    }
-   
+
    @discardableResult func heightAspect(_ value: CGFloat) -> Self {
       view.addAnchors.constHeight(value * LayoutConfig.sizeAspectCoeficient)
       return self
    }
-   
+
    @discardableResult func widthAspect(_ value: CGFloat) -> Self {
       view.addAnchors.constWidth(value * LayoutConfig.sizeAspectCoeficient)
       return self
@@ -111,49 +111,37 @@ public extension ViewSetterProtocol {
       height(CGFloat(Int.max), priority: .defaultLow)
       return self
    }
-   
+
    @discardableResult func hidden(_ value: Bool, isAnimated: Bool = false) -> Self {
       guard isAnimated else {
          view.isHidden = value
          return self
       }
-      
-      let curAlpha = view.alpha
-      if !value {
-         view.alpha = 0
-         UIView.animate(withDuration: 0.2) {
-            self.view.alpha = curAlpha
-         }
-         view.isHidden = value
-      } else {
-         UIView.animate(withDuration: 0.2) {
-            self.view.alpha = 0
-         } completion: { _ in
-            self.view.alpha = curAlpha
-            self.view.isHidden = value
-         }
+
+      UIView.animate(withDuration: 0.25) {
+         self.view.isHidden = value
+      } completion: { _ in
+         self.view.isHidden = value
       }
+
       return self
    }
-   
+
    @discardableResult func hiddenAnimated(_ isHidden: Bool, duration: CGFloat) -> Self {
-      if isHidden == false {
-         view.isHidden = false
-      }
       UIView.animate(withDuration: duration) {
-         self.view.alpha = isHidden ? 0 : 1
+         self.view.isHidden = isHidden
       } completion: { _ in
          self.view.isHidden = isHidden
       }
-      
+
       return self
    }
-   
+
    @discardableResult func alpha(_ value: CGFloat) -> Self {
       view.alpha = value
       return self
    }
-   
+
    @discardableResult func zPosition(_ value: CGFloat) -> Self {
       view.layer.masksToBounds = false
       view.clipsToBounds = false
@@ -161,12 +149,12 @@ public extension ViewSetterProtocol {
       view.setNeedsLayout()
       return self
    }
-   
+
    @discardableResult func placing(_ value: CGPoint) -> Self {
       view.center = value
       return self
    }
-   
+
    @discardableResult func shadow(_ value: Shadow) -> Self {
       view.layer.shadowColor = value.color.cgColor
       view.layer.shadowOffset = .init(width: value.offset.x, height: value.offset.y)
@@ -174,27 +162,27 @@ public extension ViewSetterProtocol {
       view.layer.shadowOpacity = Float(value.opacity)
       return self
    }
-   
+
    @discardableResult func shadowOpacity(_ value: CGFloat) -> Self {
       view.layer.shadowOpacity = Float(value)
       return self
    }
-   
+
    @discardableResult func addModel(_ value: UIViewModel, setup: ((Anchors, UIView) -> Void)? = nil) -> Self {
       let subview = value.uiView
       view.addSubview(subview)
       setup?(subview.addAnchors, view)
       return self
    }
-   
+
    @discardableResult func subviewModel(_ value: UIViewModel) -> Self {
       let subview = value.uiView
       view.addSubview(subview)
       subview.addAnchors.fitToView(view)
-      
+
       return self
    }
-   
+
    @discardableResult func subviewModels(_ value: [UIViewModel]) -> Self {
       value.forEach {
          let subview = $0.uiView
@@ -203,37 +191,37 @@ public extension ViewSetterProtocol {
       }
       return self
    }
-   
+
    @discardableResult func contentMode(_ value: UIView.ContentMode) -> Self {
       view.contentMode = value
       return self
    }
-   
+
    @discardableResult func safeAreaOffsetDisabled() -> Self {
       view.insetsLayoutMarginsFromSafeArea = false
       return self
    }
-   
+
    @discardableResult func clipsToBound(_ value: Bool) -> Self {
       view.clipsToBounds = value
       return self
    }
-   
+
    @discardableResult func maskToBounds(_ value: Bool) -> Self {
       view.layer.masksToBounds = value
       return self
    }
-   
+
    @discardableResult func removeAllConstraints() -> Self {
       view.removeAllConstraints()
       return self
    }
-   
+
    @discardableResult func userInterractionEnabled(_ value: Bool) -> Self {
       view.isUserInteractionEnabled = value
       return self
    }
-   
+
    @discardableResult func removeTapGestures() -> Self {
       for gesture in view.gestureRecognizers ?? [] {
          print(gesture)
@@ -243,19 +231,19 @@ public extension ViewSetterProtocol {
       }
       return self
    }
-   
+
    @discardableResult func cancelToucherForAllGestures(_ value: Bool) -> Self {
       for gesture in view.gestureRecognizers ?? [] {
          gesture.cancelsTouchesInView = value
       }
       return self
    }
-   
+
    @discardableResult func contentScale(_ value: CGFloat) -> Self {
       view.contentScaleFactor = value
       return self
    }
-   
+
    @discardableResult
    func startTapGestureRecognize(cancelTouch: Bool = false) -> Self where Self: Tappable, Self.View: Tappable {
       view.startTapGestureRecognize(cancelTouch: cancelTouch)
