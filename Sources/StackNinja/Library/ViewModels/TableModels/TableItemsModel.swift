6c152f7f883c922b944a6308bb41c0034bfc21d6
Yerzhan <gapurinoverzhan@gmail.com>
2023-09-13 04:41:52 +0500

    uncomment didSet { cache = [:] } for itemSections

--- Sources/StackNinja/Library/ViewModels/TableModels/TableItemsModel.swift
+++ Sources/StackNinja/Library/ViewModels/TableModels/TableItemsModel.swift
@@ -80,7 +80,7 @@ public final class TableItemsModel: BaseViewModel<TableViewExtended>,
       didSet {
          print("xela: TABLE SET\n")
          isRequestedPagination = false
-//         cache = [:]
+         cache = [:]
       }
    }
 
